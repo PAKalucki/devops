679721deb5a936e3ee22d103962e24676cc47df4
Przemyslaw Kalucki <pakalucki@gmail.com>
2025-02-17 11:50:22 +0100

    backup scripts

---
+++ scripts/utils/isitoenv.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set +e
+
+### arg 1 version
+mkdir -p $HOME/bin
+cd $HOME/bin
+
+echo "Starting setup for istio ${1}"
+if [[ -d "$HOME/bin/istio-${1}" ]]
+then
+  echo "istio-${1} is already installed, updating symlinks"
+  rm -f $HOME/bin/istioctl || true
+  ln -s $HOME/bin/istio-${1}/bin/istioctl $HOME/bin/istioctl
+else
+  echo "istio-${1} is not installed, pulling new version and updating symlinks"
+  curl -L https://istio.io/downloadIstio | ISTIO_VERSION=${1} TARGET_ARCH=x86_64 sh -
+  rm -f $HOME/bin/istioctl || true
+  ln -s $HOME/bin/istio-${1}/bin/istioctl $HOME/bin/istioctl
+fi
+echo "Setup complete"
+istioctl version
